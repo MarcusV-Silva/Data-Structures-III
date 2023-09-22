f8d7ef4e3e571ee003e72cd6353f2160484b9ee0
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-09-21 22:46:52 -0300

    initial develop/rodrigo

---
+++ initial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+// as tecnologias de origem possuem diferentes pesos para diferentes tecnologias de destino
+
+int main(){
+
+
+}
+
