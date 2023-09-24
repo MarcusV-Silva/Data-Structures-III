1c84d667b30911df88af5bbe03cd58ba7472a2d7
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-09-23 21:25:08 -0300

    adding comments

--- src/func1.c
+++ src/func1.c
@@ -2,35 +2,41 @@
 
 void funcionalidade1(char *dataCSV, char *dataBIN){
     registro *r1 =(registro *) malloc(sizeof(registro));
+    registroCab *rC = (registroCab *) malloc(sizeof(registroCab));
 
-    registroCab *rC= malloc(sizeof(registroCab));
 
-
-    FILE *csv = fopen(dataCSV, "r");
-    if(!csv){
+    FILE *csvFile = fopen(dataCSV, "r");
+    if(!csvFile){
         printf("Erro ao abrir arquivo\n");
     }
 
-    FILE *bin = fopen(dataBIN, "wb");
-    if(!bin){
+    FILE *binFile = fopen(dataBIN, "wb");
+    if(!binFile){
         printf("Erro ao abrir arquivo\n");
     }
 
+    //ignora a primeira linha do .csv
+    int c;
+    while ((c =  fgetc(csvFile)) != EOF && c != '\n') {
+    }
+
+    //inicializa registro de cabeçalho .bin
     rC->status = '1';
     rC->proxRRN = 0;
     rC->nroTecnologias = 0;
     rC->nroParesTecnologias = 0;
 
-    fwrite(rC, sizeof(registroCab), 1, bin );
+    //escreve os dados da struct de cabeçalho no .bin 
+    //(ISSUE: esta escrevendo em 3 bytes a mais)
+    fwrite(rC, sizeof(registroCab), 1, binFile);
 
-    fseek(csv, 68, SEEK_SET);
-
-    while(fread(r1, sizeof(r1), 1, csv) != 0 ){
-        fwrite(r1, sizeof(r1), 1, bin);
+    //escreve os dados do .csv no .bin
+    while(fread(r1, sizeof(r1), 1, csvFile) != 0 ){
+        fwrite(r1, sizeof(r1), 1, binFile);
     }
 
     free(r1);
     free(rC);
-    fclose(csv);
-    fclose(bin);
+    fclose(csvFile);
+    fclose(binFile);
 }
