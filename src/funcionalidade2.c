779862e9b0644a2095c3689568daae906a8501e8
Marcus Silva <marcusviniciussilva188@gmail.com>
2023-09-26 14:11:58 -0300

    Atualizando arquivos

--- src/funcionalidade2.c
+++ src/funcionalidade2.c
@@ -1,25 +1,45 @@
 #include "header.h"
 
 
-void funcionalidade2(char *dataBIn){
+void funcionalidade2(char *dataBin){
 
-    FILE *dataBInFile = fopen(dataBIn,"rb");
+    FILE *dataBInFile = fopen(dataBin,"rb");
 
     if(!dataBInFile){
         printf("Erro ao abrir arquivo\n");
         return;
     }
 
-    registroCab *rc = (registroCab*) malloc(sizeof(registroCab));
-    registro *r = malloc(sizeof(registro));
+    registro *r = (registro*) malloc(sizeof(registro));
+    //registro r;
+    //char l[256];
 
-    fread(rc,sizeof(registroCab),1,dataBInFile);
-
-    while(fread(r,sizeof(registro),1,dataBInFile) > 0){
-        //printf("%c, %d, %d, %c, %d \n", dataBIn[0], dataBIn[1], dataBIn[2], dataBIn[3], dataBIn[4]);
-    }
+    //fread(rc,sizeof(registroCab),1,dataBInFile);
+    //fseek(dataBInFile,13, SEEK_CUR);
 
     free(r);
-    free(rc);
     fclose(dataBInFile);
+}
+
+
+void lerArquivo(char *dataBinFile){
+    registro *r;
+
+    r = malloc(sizeof(registro));
+
+    registroCab *rc = (registroCab*) malloc(sizeof(registroCab));
+
+    fread(rc, sizeof(registroCab), 1, dataBinFile);
+
+    while(fread(&r.removido,1,1,dataBinFile) != 0){
+        fread(&r.grupo,sizeof(int),1,dataBinFile);
+        fread(&r.popularidade,sizeof(int), 1, dataBinFile);
+        fread(&r.peso, sizeof(int), 1, dataBinFile);
+        fread(&r.tamTecnologiaOrigem, sizeof(int),1,dataBinFile);
+        fread(&r.nmTecnologiaOrigem, sizeof(char),r.tamTecnologiaOrigem, dataBinFile);
+        fread(&r.tamTecnologiaDestino, sizeof(int),1,dataBinFile);
+        fread(&r.nmTecnologiaDestino, sizeof(char),r.tamTecnologiaDestino, dataBinFile);
+    }
+
+    free(rc);
 }
