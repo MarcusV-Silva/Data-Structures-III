93d802b225270d67ebef5e153036a532b4c0786c
RodrigoRCZ <rodrigo.rcastro27@gmail.com>
2023-11-02 14:36:01 -0300

    commit inicial

---
+++ Trabalho_T1/src/cabecalho.h
@@ -0,0 +1,15 @@
+#ifndef CABECALHO_H
+#define CABECALHO_H
+
+#define TAM_LIXO_CAB_INDEX 196
+
+// Struct referente aos dados do cabeçalho do arquivo
+typedef struct cabecalhoIndice{
+    char status;
+    int noRaiz;
+    int RRNproxNo;
+}cabIndice;
+
+
+
+#endif
